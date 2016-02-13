12650900a9a838d2685258e8565418185da2e068
Tom Flynn <tom.flynn@samsung.com>
2016-02-12 22:54:35 -0800

    start of webui
    start of webui

--- gvr-remote-scripting/src/org/gearvrf/sample/remote_scripting/GearVRScripting.java
+++ gvr-remote-scripting/src/org/gearvrf/sample/remote_scripting/GearVRScripting.java
@@ -18,6 +18,7 @@ package org.gearvrf.sample.remote_scripting;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Context;
+import android.graphics.Bitmap;
 import org.gearvrf.GVRActivity;
 import android.net.wifi.WifiManager;
 import java.nio.ByteOrder;
@@ -25,11 +26,47 @@ import java.lang.Integer;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class GearVRScripting extends GVRActivity
+import com.oculus.VRTouchPadGestureDetector;
+import com.oculus.VRTouchPadGestureDetector.OnTouchPadGestureListener;
+import com.oculus.VRTouchPadGestureDetector.SwipeDirection;
+
+public class GearVRScripting extends GVRActivity implements OnTouchPadGestureListener 
 {
+    private GearVRScriptingManager mScript;
     private final String TAG = "GearVRScripting";
     String ipAddress;
+
+
+    private static final String BROWSER_NAME = "Navigator";
+    private static final String VERSION = "0.1";
+
+    private VRTouchPadGestureDetector mDetector = null;
+
+    private static final int BUTTON_INTERVAL = 1000;
+    private static final int TAP_INTERVAL = 300;
+
+    private long mLatestButton = 0;
+    private long mLatestTap = 0;
+
+    private WebView mWebView;
+    public final int WEBVIEW_WIDTH = 1200;
+    public final int WEBVIEW_HEIGHT = 900;
+
+    private String base = "file:///android_asset/web/";
+    private String homeUrl = base+"index.html";
+
 
     /** Called when the activity is first created. */
     @Override
@@ -39,7 +76,8 @@ public class GearVRScripting extends GVRActivity
 
         ipAddress = getWifiIpAddress(this);
 
-        setScript(new GearVRScriptingManager(), "gvr.xml");
+        mScript = new GearVRScriptingManager();
+        setScript(mScript, "gvr.xml");
     }
 
     /* 
@@ -71,4 +109,272 @@ public class GearVRScripting extends GVRActivity
     public String getIpAddress() {
         return ipAddress;
     }
+
+    private void createWebView() {
+
+        mWebView = new WebView(this);
+
+        mWebView.setInitialScale(100);
+
+        int width = this.WEBVIEW_WIDTH, height = this.WEBVIEW_HEIGHT;
+
+        mWebView.measure(width, height);
+        mWebView.layout(0, 0, width, height);
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setGeolocationEnabled(true);
+
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setNeedInitialFocus(false);
+
+        String UA = settings.getUserAgentString();
+        settings.setUserAgentString(UA+" VR " + BROWSER_NAME+"/"+VERSION);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                //mScript.reset();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // inject library
+                String libSrc = "navi.js";
+
+                //mActivity.injectScript(libSrc);
+            }
+        });
+
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        mWebView.addJavascriptInterface(new WebAppInterface(this), "_NAVI");
+
+    }
+
+
+    WebView getWebView() {
+        return mWebView;
+    }
+
+    void loadHome() {
+        mWebView.loadUrl(homeUrl);
+    }
+
+    void loadUrl(String url) {
+        mWebView.loadUrl(url);
+    }
+
+    void executeJS(String js) {
+        if (!js.startsWith("javascript"))
+            js = "javascript:"+js;
+        mWebView.loadUrl(js);
+    }
+
+    void injectScript(String jsPath) {
+        String script =
+           "var s = document.createElement('script');" +
+           "s.setAttribute('src', '"+ jsPath +"');" +
+           "s.onload = function(){};" +
+           "document.body.appendChild(s);";
+
+        mWebView.loadUrl("javascript:"+script);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mWebView != null) {
+            mWebView.pauseTimers();
+        }
+        mScript.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mWebView != null) {
+            mWebView.resumeTimers();
+        }
+        mScript.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > mLatestButton + BUTTON_INTERVAL) {
+            mLatestButton = System.currentTimeMillis();
+
+            android.util.Log.v(TAG, "onBackPressed");
+
+            mScript.onButtonDown();
+        }
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        android.util.Log.v(TAG, "onLongPress");
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            mLatestButton = System.currentTimeMillis();
+
+            mScript.onLongButtonPress(keyCode);
+        }
+        return super.onKeyLongPress(keyCode, event);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            mScript.onKeyDown(keyCode, event);
+        } else if (event.getAction() == KeyEvent.ACTION_UP) {
+            mScript.onKeyUp(keyCode, event);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTap(MotionEvent event) {
+        android.util.Log.v(TAG, "onSingleTap");
+        if (System.currentTimeMillis() > mLatestTap + TAP_INTERVAL) {
+            mLatestTap = System.currentTimeMillis();
+
+            mScript.onSingleTap(event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDetector.onTouchEvent(event);
+
+        mScript.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public boolean onSwipe(MotionEvent e, SwipeDirection swipeDirection,
+            float velocityX, float velocityY) {
+        mScript.onSwipe(e, swipeDirection, velocityX, velocityY);
+        android.util.Log.v(TAG, "onSwipe");
+        return false;
+    }
+
+    // WebView interface
+    class WebAppInterface {
+        Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /*
+        // Can replace all methods with this to avoid duplication in Activity & Script?
+        // need to get all arguments... reflection or varargs or JS can encode
+        @JavascriptInterface
+        public String call(String methodName, String params) {
+            return mScript.call(methodName, params);
+        }
+
+        @JavascriptInterface
+        public String getUUID() {
+            return UUID.randomUUID().toString();
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void refreshWebView() {
+            mScript.refreshWebView();
+        }
+
+        @JavascriptInterface
+        public String getValue(String key) {
+            return mScript.getValue(key);
+        }
+
+        @JavascriptInterface
+        public void setValue(String key, String value) {
+            mScript.setValue(key, value);
+        }
+
+
+        // Background 
+        @JavascriptInterface
+        public String getBackground() {
+            return mScript.getBackground();
+        }
+
+        @JavascriptInterface
+        public void setBackground(String bg) {
+            mScript.setBackground(bg);
+        }
+
+
+        // Objects 
+        @JavascriptInterface
+        public void rotateObject(String name, float angle, float x, float y, float z) {
+            mScript.rotateObject(name, angle, x,y,z);
+        }
+
+        public void setObjectRotation(String name, float w, float x, float y, float z) {
+            mScript.setObjectRotation(name, w,x,y,z);
+        }
+
+        @JavascriptInterface
+        public void setObjectPosition(String name, float x, float y, float z) {
+            mScript.setObjectPosition(name, x,y,z);
+        }
+
+        @JavascriptInterface
+        public void translateObject(String name, float x, float y, float z) {
+            mScript.translateObject(name, x,y,z);
+        }
+
+        @JavascriptInterface
+        public void setObjectScale(String name, float x, float y, float z) {
+            mScript.setObjectScale(name, x,y,z);
+        }
+
+        @JavascriptInterface
+        public void setObjectVisible(String name, boolean visible) {
+            mScript.setObjectVisible(name, visible);
+        }
+
+        @JavascriptInterface
+        public void createObject(String name, String type) {
+            mScript.createNewObject(name, type);
+        }
+
+        // Scene 
+        @JavascriptInterface
+        public void addObjectToScene(String scene, String object) {
+            mScript.addObjectToScene(scene, object);
+        }
+
+        @JavascriptInterface
+        public void removeObjectFromScene(String scene, String object) {
+            mScript.removeObjectFromScene(scene, object);
+        }
+        */
+    }
+
+
 }
--- gvr-remote-scripting/src/org/gearvrf/sample/remote_scripting/GearVRScriptingManager.java
+++ gvr-remote-scripting/src/org/gearvrf/sample/remote_scripting/GearVRScriptingManager.java
@@ -23,9 +23,66 @@ import org.gearvrf.GVRContext;
 import org.gearvrf.GVRScene;
 import org.gearvrf.scene_objects.GVRTextViewSceneObject;
 import android.view.Gravity;
+import java.util.ArrayList;
+import java.util.List;
+import org.gearvrf.GVRAndroidResource;
+import org.gearvrf.GVRBitmapTexture;
+import org.gearvrf.GVRCameraRig;
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVREyePointeeHolder;
+import org.gearvrf.GVRMaterial;
+import org.gearvrf.GVRMesh;
+import org.gearvrf.GVRMeshEyePointee;
+import org.gearvrf.GVRPicker;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRScript;
+import org.gearvrf.GVRTexture;
+import org.gearvrf.animation.GVRAnimation;
+import org.gearvrf.animation.GVRAnimationEngine;
+import org.gearvrf.animation.GVROpacityAnimation;
+import org.gearvrf.scene_objects.GVRCameraSceneObject;
+import org.gearvrf.scene_objects.GVRCubeSceneObject;
+import org.gearvrf.scene_objects.GVRCylinderSceneObject;
+import org.gearvrf.scene_objects.GVRSphereSceneObject;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.os.SystemClock;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.oculus.VRTouchPadGestureDetector.SwipeDirection;
+
 
 public class GearVRScriptingManager extends GVRScript
 {
+    private GVRContext mContext;
+    private GearVRScripting mActivity;
+
+    private GVRScene mScene;
+    private GVRCameraRig mRig;
+    private GVRSceneObject mHeadContainer;
+
+    private List<GVRPicker.GVRPickedObject> pickedObjects;
+    private float[] hitLocation = new float[3];
+
+    private GVRSceneObject mContainer;
+
+    private Browser browser;
+    private boolean browserFocused = true;
+
+    private EditText editText;
+    private boolean activated = false;
+
     @Override
     public void onInit(GVRContext gvrContext) {
         gvrContext.startDebugServer();
@@ -52,4 +109,163 @@ public class GearVRScriptingManager extends GVRScript
     @Override
     public void onStep() {
     }
+
+    private final float mBrowserWidth  = 4f;
+    private final float mBrowserHeight = 3f;
+    
+    public void createBrowser() {
+        float distance = 3.5f;
+
+        float width  = mBrowserWidth;
+        float height = mBrowserHeight;
+
+        WebView webView = mActivity.getWebView();
+
+        browser = new Browser(mContext, mActivity, width, height, webView);
+
+        editText = browser.getEditText();
+
+        GVRSceneObject screenObject = browser.getScreenObject();
+        screenObject.setPickingEnabled(true);
+
+        browser.getSceneObject().getTransform().setPosition(0f, 0f, -distance);
+
+        mContainer.addChildObject( browser.getSceneObject() );
+
+        // add buttons to container
+        GVRSceneObject uiContainerObject = new GVRSceneObject(mContext);
+        uiContainerObject.getTransform().setPosition(-1.3f*(width/2f), 0f, -distance);
+        uiContainerObject.getTransform().rotateByAxis(20f, 0f, 1f, 0f);
+        mContainer.addChildObject(uiContainerObject);
+
+    }
+
+    public void onPause() {
+
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onKeyDown(int keyCode, KeyEvent event) {
+        if (browserFocused) {
+            WebView webView = browser.getWebView();
+            webView.dispatchKeyEvent(event);
+            return;
+        }
+
+        if (!activated) {
+            editText.setActivated(true);
+            editText.requestFocus();
+            activated = true;
+        }
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            String navText = editText.getText().toString();
+
+            if ( Patterns.WEB_URL.matcher(navText).matches() ) {
+                if (!navText.toLowerCase().startsWith("http://"))
+                    navText = "http://" + navText;
+                browser.getWebView().loadUrl(navText);
+            }
+
+            return;
+        }
+
+        editText.dispatchKeyEvent(event);
+    }
+
+    public void onKeyUp(int keyCode, KeyEvent event) {
+        if (browserFocused) {
+            WebView webView = browser.getWebView();
+            webView.dispatchKeyEvent(event);
+            return;
+        }
+
+        if (!activated) {
+            editText.setActivated(true);
+            editText.requestFocus();
+            activated = true;
+        }
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            return;
+        }
+
+        editText.dispatchKeyEvent(event);
+    }
+
+    public void click() {
+        WebView webView = browser.getWebView();
+
+        final long uMillis = SystemClock.uptimeMillis();
+
+        int width = mActivity.WEBVIEW_WIDTH,
+           height = mActivity.WEBVIEW_HEIGHT;
+
+        float hitX = this.hitLocation[0];
+        float hitY = this.hitLocation[1] * -1f;
+        
+        // normalize
+        hitX += mBrowserWidth/2.0f;
+        hitY += mBrowserHeight/2.0f;
+        hitX /= mBrowserWidth;
+        hitY /= mBrowserHeight;
+
+        //this.showMessage("coords: " + hitX + "  " + hitY);
+        
+        float x = Math.round(hitX * width);
+        float y = Math.round(hitY * height);
+
+        webView.dispatchTouchEvent(MotionEvent.obtain(uMillis, uMillis,
+                MotionEvent.ACTION_DOWN, x, y, 0));
+        webView.dispatchTouchEvent(MotionEvent.obtain(uMillis, uMillis,
+                MotionEvent.ACTION_UP, x, y, 0));
+    }
+
+    public void scroll(int direction, float velocity) {
+        WebView webView = browser.getWebView();
+
+        int dy = direction * 10;
+
+        webView.scrollBy(0, dy);
+    }
+
+    public void onSingleTap(MotionEvent event) {
+        if (browserFocused) {
+            click();
+        }     
+    }
+
+    public void onButtonDown() {
+
+    }
+
+    public void onLongButtonPress(int keyCode) {
+
+    }
+
+    public void onTouchEvent(MotionEvent event) {
+
+    }
+
+    public boolean onSwipe(MotionEvent e, SwipeDirection swipeDirection,
+            float velocityX, float velocityY) {
+
+        switch (swipeDirection) {
+            case Up:
+                scroll(-1, velocityY);
+                break;
+            case Down:
+                scroll(1, velocityY);
+                break;
+            case Forward:
+                break;
+            case Backward:
+                break;
+        }
+        return true;
+    }
+
 }
